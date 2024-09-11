f5dbf6344a88f70af3ba99f0dac2f317de0bdfa0
Hwiwon Kang <syaron9364@gmail.com>
2024-09-11 16:46:22 +0900

    update: 헤더css,모달

---
+++ src/common/ModalMain/ModalMain.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import "./ModalMain.style.css"
+
+const ModalMain = ({ isOpen, menuList, toggleMenu }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (!isMobile) return null;
+
+  return (
+    <div className={`modal-layer ${isOpen ? "open" : ""}`}>
+        <div onClick={toggleMenu} className={`btn-modal-close ${isOpen ? "open" : ""}`}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+      <div className='modal-content'>
+        <ul className={`modal-nav ${isOpen ? "active" : ""}`}>
+          {menuList.map((menu, idx) => {
+            return <li className='modal-nav-menu' key={idx}><Link className='modal-nav-a' to="/login">{menu}</Link></li>
+          })}
+        </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default ModalMain
