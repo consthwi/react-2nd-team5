146eb77c108a140ae89a83932fed35204b90e3e6
Hwiwon Kang <syaron9364@gmail.com>
2024-09-13 00:51:22 +0900

    update:홈페이지슬라이드 로딩컴포넌트 적용

--- src/pages/Homepage/component/menuslide/MenuSlide.jsx
+++ src/pages/Homepage/component/menuslide/MenuSlide.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useRecipeDataQuery } from '../../../../hooks/useRecipeData'
-import { ClipLoader } from 'react-spinners';
-import { Alert } from 'bootstrap';
-import {responsive} from '../../../../constants/responsive';
-import HomepageSlider from '../../../../common/Slider/HomepageSlider';
+import React from "react";
+import { useRecipeDataQuery } from "../../../../hooks/useRecipeData";
+import { Alert } from "bootstrap";
+import { responsive } from "../../../../constants/responsive";
+import HomepageSlider from "../../../../common/Slider/HomepageSlider";
+import LoadingGifImage from "../../../../common/LoadingGif/LoadingGif";
 
 const MenuSlide = () => {
-  const {data,isError,error,isLoading} = useRecipeDataQuery()
- if(isLoading){
-  return  <div ><ClipLoader color ="#f88c6b" loading={isLoading} size={70} /></div>
- }
- if(isError){return <Alert varient ="danger">{error.message}</Alert>}
+  const { data, isError, error, isLoading } = useRecipeDataQuery();
+  if (isLoading) {
+    return <LoadingGifImage sectionHeight={"300px"} />;
+  }
+  if (isError) {
+    return <Alert varient="danger">{error.message}</Alert>;
+  }
 
-  console.log(data)
-  return (
-   <HomepageSlider data={data} responsive={responsive}/>
-  )
-}
+  console.log(data);
+  return <HomepageSlider data={data} responsive={responsive} />;
+};
 
-export default MenuSlide 
+export default MenuSlide;
