873625ae24412eae3cef8545ac1df53b926faf9d
dante0214 <rn_chic@naver.com>
2024-09-12 13:06:09 +0900

    feat: api변경

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import RecipePage from "./pages/RecipePage/RecipePage";
 import AppLayout from "./common/layout/AppLayout/AppLayout";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import "./common/styles/font.css";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
 import RecipeDetailPage from "./pages/RecipeDetailPage/RecipeDetailPage";
 
 function App() {
   return (
-<Routes>
-  <Route path="/" element={<AppLayout />}>
-    <Route index element={<HomePage/>} />
-    <Route path="recipes" element={<RecipePage />} />
-    <Route path=":id" element={<RecipeDetailPage />} />
-  </Route>
-  <Route path="*" element={<NotFoundPage />} />
-</Routes>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+
+        <Route path="recipes">
+          <Route index element={<RecipePage />} />
+          <Route path=":recipeName" element={<RecipeDetailPage />} />
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
 
