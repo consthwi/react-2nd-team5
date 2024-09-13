6f169404d69b12b5d661b3bb8f6909b8ffa216e5
Byunh0 <bhy1999313@naver.com>
2024-09-13 18:33:29 +0900

    fix:homepage 수정

--- src/common/card/SliderCard.jsx
+++ src/common/card/SliderCard.jsx
@@ -1,45 +1,71 @@
-import React from 'react'
-import "./SliderCard.style.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUtensils } from '@fortawesome/free-solid-svg-icons'
-import { useNavigate } from 'react-router-dom'
-import { Card, Col, Row } from 'react-bootstrap'
+import React, { useState } from 'react';
+import "./SliderCard.style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { Card, Col, Row } from 'react-bootstrap';
 import { PiBookmarkSimpleThin, PiBookmarkSimpleFill } from "react-icons/pi";
-const SliderCard = ({recipeitem,key}) => {
+
+const SliderCard = ({ recipeitem, key }) => {
     const navigate = useNavigate();
-    const godetail=()=>{
-        navigate(`/recipes/${recipeitem?.RCP_NM}`)
+    const [bookmarkStates, setBookmarkStates] = useState({});
+
     
-    }
-  return (
-    // <div style={{backgroundImage:"url("+`${recipeitem?.ATT_FILE_NO_MAIN}`+")"}} className="slidercard_url" onClick={godetail}>
-    //     <div className="overlay">
-    //     <div className="slidercard_rcp_nm"><FontAwesomeIcon icon={faUtensils} /> {recipeitem?.RCP_NM}</div>
-    //     <div>{recipeitem?.RCP_SEQ}</div>
-    //     <div>{recipeitem?.RCP_WAY2}</div>
-    //     </div>
-    // </div>
-    <Row xs={1} md={2} className="g-4">
-    
-      <Col>
-        <Card onClick={godetail} className="slidercard_url">
-        <Card.Img variant="top" src={recipeitem?.ATT_FILE_NO_MAIN} className="card_img"/>
-          <Card.Body className="card_text">
-            <Card.Title className="slidercard_rcp_nm"><FontAwesomeIcon icon={faUtensils} /> {recipeitem?.RCP_NM}</Card.Title>
-            <Card.Text >
-            {/* <div className="recipeitem">{recipeitem?.HASH_TAG}</div> */}
-            <PiBookmarkSimpleThin
-                  className="bookmark-icon-inSlidercard"
-                  size="3rem"
-                />
-            <div className="recipeitem">{recipeitem?.RCP_WAY2}</div>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+    const beBookmarked = bookmarkStates[key] || false;
+
   
-  </Row>
-  )
+    const godetail = () => {
+        navigate(`/recipes/${recipeitem?.RCP_NM}`);
+    }
+
+    // 토글
+    const handleToggleBookmark = (event) => {
+        event.stopPropagation(); 
+        setBookmarkStates((prevStates) => {
+            const newState = { ...prevStates };
+            newState[key] = !newState[key]; 
+            return newState;
+        });
+    }
+
+    return (
+        <Row xs={1} md={2} className="g-4">
+            <Col>
+                <Card className="slidercard_url">
+                    <Card.Img 
+                        variant="top" 
+                        src={recipeitem?.ATT_FILE_NO_MAIN} 
+                        className="card_img" 
+                        onClick={godetail}
+                    />
+                    <Card.Body className="card_text">
+                        <Card.Title 
+                            className="slidercard_rcp_nm"  
+                            onClick={godetail}
+                        >
+                            <FontAwesomeIcon icon={faUtensils} /> {recipeitem?.RCP_NM}
+                        </Card.Title>
+                        <Card.Text>
+                            {beBookmarked ? (
+                                <PiBookmarkSimpleFill
+                                    className="bookmark-icon-inSlidercard"
+                                    size="3rem"
+                                    onClick={handleToggleBookmark}
+                                />
+                            ) : (
+                                <PiBookmarkSimpleThin
+                                    className="bookmark-icon-inSlidercard"
+                                    size="3rem"
+                                    onClick={handleToggleBookmark}
+                                />
+                            )}
+                            <div className="recipeitem">{recipeitem?.RCP_WAY2}</div>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    )
 }
 
-export default SliderCard
+export default SliderCard;
