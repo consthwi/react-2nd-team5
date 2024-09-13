af9d83f5c7246201fe1aadf31686e5a03f0ecae8
dante0214 <rn_chic@naver.com>
2024-09-13 16:19:36 +0900

    feat: 디자인 변경

--- src/pages/RecipePage/components/SelectMenu.jsx
+++ src/pages/RecipePage/components/SelectMenu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-
+import "./SelectMenu.style.css";
 const SelectMenu = ({ selectValue, handleSelectChange }) => {
   return (
     <Form.Group>
