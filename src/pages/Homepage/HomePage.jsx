9654efe1c00b96f382b70fa4f08e4062264dd7cd
minsun283 <10000.hohoho@gmail.com>
2024-09-14 18:29:35 +0900

    feat:homepage top 배너 수정+디테일페이지넘버링

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -6,6 +6,7 @@ import { IoSearch } from "react-icons/io5";
 import TopSlide from "./component/TopSlide/TopSlide";
 import { useNavigate } from "react-router-dom";
 import BookmarkInfo from "./component/bookmarkslide/BookmarkInfo";
+import { Form } from "react-bootstrap";
 
 const HomePage = () => {
   const [keyword, setKeyword] = useState("");
@@ -17,14 +18,24 @@ const HomePage = () => {
   };
   return (
     <div>
-      <div className="top-area">
+      <div className="top-box-area">
         <div className="top-text">
           냉장고 속 재료로
           <br /> 레시피를 검색하세요!
         </div>
         <div className="input-box">
-          <input></input>
-          <IoSearch size="26px" color="#ed0c0c" className="mb-2" />
+          <Form className="d-flex" onSubmit={searchByKeyword}>
+            <input
+              type="search"
+              className="me-2"
+              aria-label="Search"
+              value={keyword}
+              onChange={(event) => setKeyword(event.target.value)}
+            />
+            <button className="top-search-button" type="submit">
+              <IoSearch size="26px" color="#ed0c0c" className="mb-1 mr-3" />
+            </button>
+          </Form>
         </div>
       </div>
       <div className="top-slide-area">
